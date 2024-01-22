ab5d238c7a3f515bcb0c0afd0d30e6bdb60325b3
LOAN Ngo <mrs.loan1999@gmail.com>
2024-01-22 09:57:27 +0700

    Fixed: Loop click

--- package/click/basic.py
+++ package/click/basic.py
@@ -3,14 +3,13 @@ import time
 import random
 
 def randomClick():
-    while(True):
-        # Random position
-        x, y = random.randrange(100, 1500), random.randrange(100, 1000)
+    # Random position
+    x, y = random.randrange(100, 1500), random.randrange(100, 1000)
 
-        # move mouse to that position
-        pyautogui.moveTo(x, y)
-        # click
-        pyautogui.click()
-        # time delay for action
-        randomTime = random.randrange(60,120)
-        time.sleep(randomTime) 
+    # move mouse to that position
+    pyautogui.moveTo(x, y)
+    # click
+    pyautogui.click()
+    # time delay for action
+    randomTime = random.randrange(60,120)
+    time.sleep(randomTime)  
